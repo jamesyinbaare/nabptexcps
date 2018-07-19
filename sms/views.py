2cbd4c9247af16dd10e0e287e4334bff2aede95e
=James Yin <jamesyinbaare@gmail.com>
2018-07-19 20:25:34 +0000

    base template for dashboard added

--- sms/views.py
+++ sms/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def dashboard(request):
-    return render(request, 'sms/base.html', {'greet': 'hello world'})
+    return render(request, 'sms/dashboard_index.html', {'greet': 'hello world'})
