1675a1016d18a70fdcb13eb4bb03ccb89ace240e
=James Yin <jamesyinbaare@gmail.com>
2018-07-20 17:29:38 +0000

    fix dashboard nav and main content issue

--- sms/urls.py
+++ sms/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'sms'
+
 urlpatterns = [
     path('',views.dashboard, name='dashboard' ),
+    path('dashboard/students',views.manage_students, name='dashboard_manage_students' ),
     
 ]
--- sms/views.py
+++ sms/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def dashboard(request):
     return render(request, 'sms/dashboard_index.html', {'greet': 'hello world'})
+
+
+
+def manage_students(request):
+
+    return render(request, 'sms/dashboard_manage_students.html',{})
